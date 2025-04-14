f151eaeda9f48cbab668b4b4d59700b0754a9d0b
Stanley Williams <stanley.williams-3@selu.edu>
2025-04-14 18:23:52 -0500

    fix: naming scheme

--- src/sounds/soundEffects/sound_manager.py
+++ src/sounds/soundEffects/sound_manager.py
@@ -7,7 +7,7 @@ class SoundManager:
 
         # Existing sounds dictionary
         self.sounds = {
-            "hit": pygame.mixer.Sound("src/sounds/soundEffects/large-underwater-explosion-190270.mp3"),
+            "hit": pygame.mixer.Sound("src/sounds/soundEffects/hit.mp3"),
             "miss": pygame.mixer.Sound("assets/sounds/miss.wav"),
             "ship_sunk": pygame.mixer.Sound("assets/sounds/ship_sunk.wav"),
             "button_press": pygame.mixer.Sound("assets/sounds/button_press.wav"),
@@ -15,7 +15,7 @@ class SoundManager:
         }
 
         # Background music
-        pygame.mixer.music.load("src/sounds/soundEffects/ultimatum-120bpm-orchestra-loop-325053.mp3")
+        pygame.mixer.music.load("src/sounds/soundEffects/background.mp3")
 
     def play_sound(self, sound_name):
         
